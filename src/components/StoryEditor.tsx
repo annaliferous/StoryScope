985adf439dc118eda44528dd92ed86292d772fb2
Louis <tagnacht2@gmail.com>
2025-11-16 13:41:58 +0100

    added basic editor capabilities

--- src/components/StoryEditor.tsx
+++ src/components/StoryEditor.tsx
@@ -1,27 +1,29 @@
 import { common } from "@mui/material/colors";
 
-interface StoryEditorProps {
-    doc: XMLDocument
-    onChange: (doc: XMLDocument) => void
-}
-
 /**
  * Renders a list of XML children recursively.
  */
-function StoryBlocks({ docs }: { docs: NodeListOf<ChildNode> }) {
+function StoryBlocks({ docs, onChange }: { docs: NodeListOf<ChildNode>, onChange: (doc: ChildNode) => void }) {
     return <>
         {/* {element.nodeName} */}
-        {Array.from(docs).map((child) => <StoryBlock doc={child} />)}
+        {Array.from(docs).map((child) => <StoryBlock doc={child} onChange={() => onChange(child)} />)}
     </>;
 }
 
 /**
  * Applies styles to XML nodes, given an XML doc.
  */
-function StoryBlock({ doc }: { doc: ChildNode | null }) {
+function StoryBlock({ doc, onChange }: { doc: ChildNode | null, onChange: () => void }) {
     if (!doc) return;
     console.log(doc);
-    if (doc.nodeType === doc.TEXT_NODE) return doc.textContent;
+    if (doc.nodeType === doc.TEXT_NODE) {
+        return <span contentEditable onInput={(e) => {
+            const newContent = e.currentTarget.textContent;
+
+            doc.textContent = newContent;
+            onChange();
+        }}>{doc.textContent}</span>;
+    }
 
     if (doc.nodeType !== doc.ELEMENT_NODE) return;
     const element = doc as Element;
@@ -29,20 +31,28 @@ function StoryBlock({ doc }: { doc: ChildNode | null }) {
     const type = element.getAttribute("Type");
     switch (type) {
         case "Action":
-            return <em><StoryBlocks docs={element.childNodes} /></em>;
+            return <div style={{ fontStyle: 'italic' }}><StoryBlocks docs={element.childNodes} onChange={onChange} /></div>;
         case "Dialogue":
-            return <div><StoryBlocks docs={element.childNodes} /></div>;
+            return <div><StoryBlocks docs={element.childNodes} onChange={onChange} /></div>;
         case "Character":
-            return <div style={{ paddingTop: 12, textDecoration: "underline" }}><StoryBlocks docs={element.childNodes} /></div>;
+            return <div style={{ paddingTop: 12, textDecoration: "underline" }}><StoryBlocks docs={element.childNodes} onChange={onChange} /></div>;
         case "Scene Heading":
-            return <div style={{ paddingTop: 12 }}><b><StoryBlocks docs={element.childNodes} /></b></div>;
+            return <div style={{ paddingTop: 12 }}><b><StoryBlocks docs={element.childNodes} onChange={onChange} /></b></div>;
         default:
             break;
     }
 
-    return <StoryBlocks docs={element.childNodes} />;
+    return <StoryBlocks docs={element.childNodes} onChange={onChange} />;
 }
 
+interface StoryEditorProps {
+    doc: XMLDocument
+    onChange: (doc: XMLDocument) => void
+}
+
+/**
+ * Text Editor component which renders the given XMLDocument in .fdx with a bit of markup.
+ */
 export function StoryEditor({ doc, onChange }: StoryEditorProps) {
     const $content = doc.getElementsByTagName("Content");
     if (!$content) return;
@@ -54,6 +64,6 @@ export function StoryEditor({ doc, onChange }: StoryEditorProps) {
         color: common.black,
         padding: 12,
     }}>
-        <StoryBlock doc={$content.item(0)} />
+        <StoryBlock doc={$content.item(0)} onChange={() => onChange(doc)} />
     </div>;
 }
