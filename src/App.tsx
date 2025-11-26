bd040ce49867ea552918bb0cb978b050cb391bb7
Melanie <135548773+EmlMeml@users.noreply.github.com>
2025-11-26 09:24:37 +0100

    Ersten Design Ideen/Grundstruktur-Elemente

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useRef, useState } from "react";
 import { useScreenplay } from "./hooks/useScreenplay";
 import WelcomeDialog from "./components/WelcomeDialog";
-import { Grid, Stack } from "@mui/material";
+import { Button, Grid, Stack } from "@mui/material";
+import FilterListIcon from '@mui/icons-material/FilterList';
 import { scrollStoryEditorTo, StoryEditor } from "./components/StoryEditor";
 import { Timeline } from "./components/Timeline";
+import { FilterList, Padding } from "@mui/icons-material";
 
 const TIMELINE_HEIGHT = 64;
 
@@ -18,15 +20,32 @@ function App() {
     <>
       <WelcomeDialog isOpen={!fdxFileUrl} onChange={setFdxFileUrl} />
       <Stack>
-        <Grid size={12} padding={0} height={TIMELINE_HEIGHT + "px"} overflow="scroll">
-          {screenplay && <Timeline doc={screenplay.document} height={TIMELINE_HEIGHT} onClick={(scene) => {
-            scrollStoryEditorTo(editorRef, scene.id);
-          }} />}
+        <Grid>
+          <div style={{textAlign:"center", background:"#596490", margin:0,padding:1, color:"white"}}>
+            <h1>StoryScope - Visualize Your Drama!</h1>
+          </div>
+        </Grid>
+        <Grid size={12} padding={0} height={TIMELINE_HEIGHT*1.6 + "px"} overflow="scroll">
+          <div style={{marginBottom:0, marginTop:0,background:"#BAC5EF"}}>
+            <h2 style={{marginBottom:0, marginTop:0, fontSize:16,}}>Script Navigation</h2>
+            {screenplay && <Timeline doc={screenplay.document} height={TIMELINE_HEIGHT} onClick={(scene) => {
+              scrollStoryEditorTo(editorRef, scene.id);
+            }} />}
+          </div>
         </Grid>
         <Grid container height={`calc(100vh - ${TIMELINE_HEIGHT}px)`} overflow="scroll">
           <Grid size={6}>
-            Visualisations will be here soon!
-            You've read {(editorOffset * 100).toFixed(2)}% of the script.
+            <div id="buttonHeader" style={{height:"10%"}}> 
+              <h3>Drama - Visualization</h3>
+              <Button variant="contained" startIcon={<FilterListIcon/>} size="large"></Button>
+              <Button variant="contained">Relationship - Overview</Button>
+              <Button variant="contained">Location - Overview</Button>
+            </div>
+            <div id="content" style={{background:"#e0e0e0ff", height:"90%"}}>
+              Visualisations will be here soon!
+              You've read {(editorOffset * 100).toFixed(2)}% of the script.
+            </div>
+            
           </Grid>
           <Grid size={6} height={`calc(100vh - ${TIMELINE_HEIGHT}px)`}>
             {screenplay && <StoryEditor ref={editorRef} doc={screenplay.document} onChange={console.log} onScroll={setEditorOffset} />}
